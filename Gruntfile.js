67803dc666a445b831b74e8b2500d633ff4bf71d
benjiaoxz <445088977@qq.com>
2016-08-18 23:06:27 +0800

    edit some

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', ['jshint']);
