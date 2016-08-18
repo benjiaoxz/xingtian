b9c9acac6382acc1ceeb488e594ae0d53faa26e8
benjiaoxz <445088977@qq.com>
2016-08-19 00:10:27 +0800

    hidden index about change to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		res.render('index');
-	});
-
-	app.get('/about', function(req, res) {
 		res.render('about');
 	});
+
+	// app.get('/about', function(req, res) {
+	// 	res.render('about');
+	// });
 };
